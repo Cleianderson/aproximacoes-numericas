045303de21eeade5d6226631f24ca10ad566b072
Cleianderson <cleianderson13@gmail.com>
2021-04-10 12:33:14 -0300

    feat: tui.py now plot solutions

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import sympy as sp
 import numpy as np
 import matplotlib.pyplot as plt
+from types import FunctionType
+
 from matplotlib.patches import Patch
 from math import *
 from metodos import *
@@ -10,24 +12,60 @@ sen = sin
 
 class ProblemInitialValue:
     fn: str
+    g: str
+    t0: float
+    y0: float
+    sol_lambda: FunctionType
 
-    def __init__(self, fn):
+    def __init__(self, fn, g, t0, y0):
         self.fn = fn
+        self.g = g
+        self.t0 = t0
+        self.y0 = y0
 
-    def solve(self, t0, y0, g):
+    def solve(self):
         t = sp.symbols('t')
         y = sp.Function('y')(t)
         f = sp.parse_expr(self.fn, {'t': t, 'y': y})
-        _g = sp.parse_expr(g, {'t': t, 'y': y})
+        _g = sp.parse_expr(self.g, {'t': t, 'y': y})
         dydt = y.diff(t)
         eq = sp.Eq(dydt - f, _g)
         dsol = sp.dsolve(eq)
-        condicion = dsol.subs(t, t0).replace(y.subs(t, t0), y0)
+        condicion = dsol.subs(t, self.t0).replace(y.subs(t, self.t0), self.y0)
         c = sp.solve(condicion)
         C1 = sp.Symbol('C1')
         sol = sp.simplify(dsol.replace(C1, c[0]))
         y_t = sp.lambdify(t, sol.args[1])
+        self.sol_lambda = y_t
         return y_t, sol
+
+    def plot(self, amp, a=0, b=10, n=8):
+        fig, ax = plt.subplots()
+
+        _fn = lambda t, y: eval(self.fn) + eval(self.g)
+
+        euler = Euler(_fn, self.t0, self.y0)
+        euler_melhor = EulerMelhorado(_fn, self.t0, self.y0)
+        runge_kutta = RungeKutta(_fn, self.t0, self.y0)
+
+        X = np.linspace(a, b, n)
+
+        y_euler = [euler.approach_to(x, amp) for x in X]
+        y_euler_melhor = [euler_melhor.approach_to(x, amp) for x in X]
+        y_runge_kutta = [runge_kutta.approach_to(x, amp) for x in X]
+
+        ax.plot(X, self.sol_lambda(X), '-o', color='green')
+        ax.plot(X, y_euler, '-o', color='red')
+        ax.plot(X, y_euler_melhor, '-o', color='purple')
+        ax.plot(X, y_runge_kutta, '-o', color='blue')
+
+        sol_patch = Patch(color='green', label='Solução')
+        e_patch = Patch(color='red', label='Método de Euler')
+        e_m_patch = Patch(color='purple', label='Método de Euler Melhorado')
+        rk_patch = Patch(color='blue', label='Método de Runge-Kutta')
+        plt.legend(handles=[sol_patch, e_patch, e_m_patch, rk_patch])
+
+        plt.show()
 
 
 def main():
--- tui.py
+++ tui.py
@@ -1,70 +1,85 @@
 #!/usr/bin/env python3
 import npyscreen
 import curses
+
 from metodos import *
+from main import ProblemInitialValue
 
 
 class App(npyscreen.StandardApp):
     def onStart(self):
         stdscr = curses.initscr()
         self.rows, self.cols = stdscr.getmaxyx()
-        self.addForm('MAIN',
-                     MainForm,
-                     name='Aproximações Numéricas',
-                     minimum_columns=self.cols,
-                     minimum_lines=self.rows)
+        self.addForm('MAIN', MainForm, name='Aproximações Numéricas')
+
+    
+    def onInMainLoop(self):
+        self.rows, self.cols = stdscr.getmaxyx()
+        self.getForm('Main').cols = self.cols
 
 
-class MainForm(npyscreen.ActionFormV2WithMenus):
+class MainForm(npyscreen.FormBaseNewWithMenus):
     def __init__(self, *args, **kwords):
         super(MainForm, self).__init__(*args, **kwords)
         self.CANCEL_BUTTON_TEXT = 'Calcular'
         self.OK_BUTTON_TEXT = 'Sair'
+        self.FIX_MINIMUM_SIZE_WHEN_CREATED = False
 
     def create(self):
-        y, x = self.curses_pad.getmaxyx()
 
-        self.fn = self.add(TitleTxt, name='Função f(t,y)')
+        self.quit = self.add_menu(name='Ações')
+        self.quit.addItem("Exit Application", self.on_exit, "e")
+        self.quit.addItem("Calculate", self.btn_press, "c")
+        self.quit.addItem("Plot", self.plot, "p")
+
+        self.fn = self.add(TitleTxt, name='f(t,y)')
+        self.g = self.add(TitleTxt, name='g(t,y)')
         self.delta_h = self.add(TitleTxt, name='Delta t')
         self.t0 = self.add(TitleTxt, name='t0')
         self.y0 = self.add(TitleTxt, name='y0')
         self.point = self.add(TitleTxt, name='Ponto')
-        self.output1 = self.add(OutputText, name='Método de Euler')
-        self.output2 = self.add(OutputText, name='Método de Euler Melhorado')
-        self.output3 = self.add(OutputText, name='Runge-Kutta')
-
-        self.btn_ok = self.add(npyscreen.MiniButtonPress,
-                               name='calcular',
-                               rely=-4)
-        self.btn_cancel = self.add(npyscreen.MiniButtonPress,
-                                   name='sair',
-                                   rely=-4,
-                                   relx=15)
-        self.btn_cancel.when_pressed_function = self.on_exit
-        self.btn_ok.when_pressed_function = self.btn_press
+        self.result = self.add(npyscreen.BoxTitle,
+                               name='Aproximações',
+                               editable=False)
 
     # Override method that triggers when you click the 'ok'
     def btn_press(self):
-        fn = lambda t, y: eval(self.fn.value)
-        t0 = float(self.t0.value)
-        y0 = float(self.y0.value)
-        point = float(self.point.value)
-        amp = float(self.delta_h.value)
+        try:
+            fn = lambda t, y: eval(self.fn.value)
+            t0 = float(self.t0.value)
+            y0 = float(self.y0.value)
+            point = float(self.point.value)
+            amp = float(self.delta_h.value)
 
-        euler = Euler(fn, t0, y0)
-        euler_melhor = EulerMelhorado(fn, t0, y0)
-        rk = RungeKutta(fn, t0, y0)
+            euler = Euler(fn, t0, y0)
+            euler_melhor = EulerMelhorado(fn, t0, y0)
+            rk = RungeKutta(fn, t0, y0)
 
-        self.output1.value = euler.approach_to(point, amp)
-        self.output2.value = euler_melhor.approach_to(point, amp)
-        self.output3.value = rk.approach_to(point, amp)
-        self.display()
+            self.result.values = [
+                f'Método de Euler                  {euler.approach_to(point, amp)}',
+                f'Método de Euler Melhorado        {euler_melhor.approach_to(point, amp)}',
+                f'Método de Runge-Kutta            {rk.approach_to(point, amp)}',
+            ]
+            self.display()
+        except ValueError:
+            self.do_nothing()
 
     # Override method that triggers when you click the 'cancel'
-    def on_exit(self):
+    def on_exit(self, *args):
         self.parentApp.setNextForm(None)
         self.exit_editing()
         # self.display()
+
+    def plot(self):
+        try:
+            amp = float(self.delta_h.value)
+            t0, y0 = float(self.t0.value), float(self.y0.value)
+            f, g = self.fn.value, self.g.value
+            problem = ProblemInitialValue(f, g, t0, y0)
+            problem.solve()
+            problem.plot(amp)
+        except ValueError:
+            self.do_nothing()
 
 
 class TitleTxt(npyscreen.TitleText):
